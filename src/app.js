d5ab773c4b68191af5038f5b176a42e1c5574744
Francisco Lopez <82164792+franciscols2011@users.noreply.github.com>
2023-12-12 01:59:02 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let simbolo_carta = ["♦", "♥", "♠", "♣"];
+let valor_carta = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+function cartasAleatorias() {
+  function elementoArray(e) {
+    return e[Math.floor(Math.random() * e.length)];
+  }
+
+  let numeroAleatorio = document.getElementById("numeroCentro");
+  let simboloCarta = document.querySelector("#elementoArriba");
+  let simboloCarta2 = document.getElementById("elementoAbajo");
+
+  numeroAleatorio.innerHTML = elementoArray(valor_carta);
+  simboloCarta.innerHTML = elementoArray(simbolo_carta);
+  simboloCarta2.innerHTML = simboloCarta.innerHTML;
+
+  if (simboloCarta.innerHTML == "♦" || simboloCarta.innerHTML == "♥") {
+    document.getElementById("elementoArriba").classList.add("rojo");
+    document.getElementById("elementoAbajo").classList.add("rojo");
+    document.getElementById("numeroCentro").classList.add("rojo");
+  } else {
+    document.getElementById("elementoArriba").classList.remove("rojo");
+    document.getElementById("elementoAbajo").classList.remove("rojo");
+    document.getElementById("numeroCentro").classList.remove("rojo");
+  }
+}
+
+cartasAleatorias();
